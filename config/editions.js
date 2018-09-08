2c716e25baeb7c0f4eb590503e297d92b6bca26e
Abraham Samma <sammadiscerning@gmail.com>
2018-09-09 02:02:53 +0300

    Add v0.5.20 empty URL

--- config/editions.js
+++ config/editions.js
@@ -1,3 +1,3 @@
-var EMPTY_URL = 'https://cdn.rawgit.com/abesamma/TW5-editions/78846468/empty.html';
+var EMPTY_URL = 'https://cdn.rawgit.com/abesamma/TW5-editions/f20ce075/empty.html';
 
 module.exports.emptyUrl = EMPTY_URL;
