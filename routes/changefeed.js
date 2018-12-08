8f0e4d4195f422b1cb72fa3349715b12f6643cdc
Abraham Samma <sammadiscerning@gmail.com>
2018-12-08 20:45:04 +0300

    Revert to aa6107e5d6a184f8d19bf940ded37bd2e1ec2281

--- routes/changefeed.js
+++ routes/changefeed.js
@@ -1,8 +1,37 @@
 var express = require('express');
 var router = express.Router();
+<<<<<<< HEAD
 
 router.get('/', function (req, res) {
     res.sendStatus(410);
+=======
+var EventSource = require('eventsource');
+var config = require('../config/secret.json');
+
+router.get('/', function (req, res) {
+    if (req.user) {
+        var doc_id = req.user.id;
+        var url = config.database + '/maarfapad/_changes?id=' +
+                  doc_id + '&feed=eventsource&filter=user/filterId&heartbeat=3000&include_docs=true';
+        res.writeHead(200,{
+            'Content-Type': 'text/event-stream',
+            'Transfer-Encoding': 'chunked',
+            'Cache-Control': 'no-cache'
+        });
+        var source = new EventSource(url);
+        source.onmessage = function (e) {
+            var data = JSON.parse(e.data);
+            var attachments = {};
+            for (var wiki in data.doc._attachments) {
+                attachments[wiki] = data.doc._attachments[wiki].revpos.toString();
+            }
+            var json = JSON.stringify(attachments);
+            res.write("data: " + json + '\n\n');
+        };
+    } else {
+        res.sendStatus(403);
+    }
+>>>>>>> parent of aa6107e... changefeed.js refactor
 });
 
 module.exports = router;
