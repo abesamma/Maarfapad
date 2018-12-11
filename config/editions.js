1116dfc8376be6e41d89823cde441be6b6b80a0f
Abraham Samma <sammadiscerning@gmail.com>
2018-12-11 19:39:59 +0300

    Tutorial hotfix for empty.html

--- config/editions.js
+++ config/editions.js
@@ -1,3 +1,3 @@
-var EMPTY_URL = 'https://rawcdn.githack.com/abesamma/TW5-editions/3e496e4771c5fc8577cd7ca395de579883ff918e/empty.html';
+var EMPTY_URL = 'https://rawcdn.githack.com/abesamma/TW5-editions/e9187079fb9e01e22ebdeaeaa7a7c0f4a0ea42a3/empty.html';
 
 module.exports.emptyUrl = EMPTY_URL;
