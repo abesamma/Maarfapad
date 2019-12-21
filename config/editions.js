4de189b8ed56ad8e3cc01f4b6a36dab0c0291a36
Abraham Samma <sammadiscerning@gmail.com>
2019-12-21 14:44:32 +0300

    Update emptyURL with latest edition url

--- config/editions.js
+++ config/editions.js
@@ -1,3 +1,3 @@
-var EMPTY_URL = 'https://rawcdn.githack.com/abesamma/TW5-editions/e9187079fb9e01e22ebdeaeaa7a7c0f4a0ea42a3/empty.html';
+var EMPTY_URL = 'https://rawcdn.githack.com/abesamma/TW5-editions/e931e63e7a1afcc5a2b0ab7d30d4fc8502089407/empty.html';
 
 module.exports.emptyUrl = EMPTY_URL;
